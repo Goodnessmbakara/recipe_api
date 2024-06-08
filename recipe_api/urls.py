8059adff917470c45c4536414112a98abd73fbff
Goodnessmbakara <amicablembakara50@gmail.com>
2024-06-08 16:29:47 +0100

    added a new serializer

--- recipe_api/urls.py
+++ recipe_api/urls.py
@@ -19,7 +19,7 @@ from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -32,6 +32,7 @@ schema_view = get_schema_view(
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
+   authentication_classes=(JWTAuthentication,),
    
 )
 
--- src/serializers.py
+++ src/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import Recipe
+from membership.serializers import MemberSerializer
 
 class RecipeSerializer(serializers.ModelSerializer):
+    created_by = MemberSerializer()
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'ingredients', 'instructions', 'created_by']
